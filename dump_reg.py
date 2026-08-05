4b2f0c855cfd7ce16c360c56fbc2bed240c125d0
parimalrane <parimalrane@gmail.com>
2026-08-05 16:04:46 +0530

    04th august output

--- dump_reg.py
+++
@@ -1,8 +0,0 @@
-import json
-try:
-    with open('market_data/stock_transition/registry.json', 'r') as f:
-        reg = json.load(f)
-        for t in ["EXTR", "SNDK", "MU", "ESTA", "HPE", "IQV"]:
-            print(f"{t}: {reg.get(t)}")
-except Exception as e:
-    print(e)
